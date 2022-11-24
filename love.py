3fbff5f460676c3a48bf2fb3de88c87a65f3c932
Federico Vega - HP <federico_Vega22@hotmail.com>
2022-11-24 20:57:59 -0300

    a simple heart for my girlfriend Lucía

---
+++ love.py
@@ -0,0 +1,76 @@
+import turtle
+from math import sqrt
+label = turtle.Turtle()
+
+label.speed(100)
+label.color("purple")
+label.pensize(5)
+label.shape("turtle")
+label.right(90)
+label.forward(150)
+label.left(90)
+label.forward(90)
+label.left(90)
+label.forward(30)
+
+for i in range(4):
+    label.left(135)
+    label.forward(sqrt((30**2)+(30**2)))
+    label.right(45)
+    label.forward(15)
+    label.right(135)
+    label.forward(sqrt((30**2)+(30**2)))
+    label.left(45)
+
+label.forward(150-30)
+label.left(90)
+label.forward(30)
+label.left(90)
+label.forward(30)
+
+for i in range(8):
+    label.left(135)
+    label.forward(sqrt((30**2)+(30**2)))
+    label.right(135)
+    label.forward(15)
+    label.right(45)
+    label.forward(sqrt((30**2)+(30**2)))
+    label.left(45)
+
+label.left(90)
+label.forward(90)
+label.left(90)
+label.forward(30)
+label.left(90)
+label.forward(60)
+label.backward(60)
+label.left(90)
+label.forward(30)
+label.right(90)
+label.forward(45)
+label.left(90)
+label.pensize(0)
+label.forward(80)
+label.left(115)
+label.color("red")
+label.pensize(5)
+
+for i in range(65):
+    label.forward(5.2)
+    label.left(1)
+
+for i in range(180):
+    label.forward(1.5)
+    label.left(1)
+
+label.right(180)
+
+for i in range(180):
+    label.forward(1.5)
+    label.left(1)
+
+for i in range(65):
+    label.forward(5.2)
+    label.left(1)
+    
+turtle.mainloop()
